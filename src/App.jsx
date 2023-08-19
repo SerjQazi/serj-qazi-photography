06b8dcf8df2b067a756e197ab7a7e73213ccae58
Serj Qazi <serjqazi@Serjs-MacBook-Pro.local>
2023-08-18 21:01:55 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1 className="m-4 p-10 text-3xl font-bold underline">Hello world!</h1>
+
+      <h2>Supreme</h2>
+    </>
+  );
+}
+
+export default App;
