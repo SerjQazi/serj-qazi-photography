84ad8e1abecf02b3cd2f6cb19e9c88ffab222560
Serj Qazi <serjqazi@Serjs-MacBook-Pro.local>
2023-08-18 23:28:53 -0400

    asset upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <>
+      <Header />
       <h1 className="m-4 p-10 text-3xl font-bold underline">Hello world!</h1>
 
       <h2>Supreme</h2>
