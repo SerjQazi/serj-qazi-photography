c1fad68904dcf93d39341ca25d06bb9745c7446f
Serj Qazi <serjqazi@Serjs-MacBook-Pro.local>
2023-08-26 03:54:58 -0400

    loop through filmStrip JSON

--- src/components/FilmStrip.jsx
+++ src/components/FilmStrip.jsx
@@ -1,147 +1,57 @@
-import images from '../../public/images/index.js';
-
+import data from '../../public/images/images.json';
 const FilmStrip = () => {
+  console.log(data);
+
   return (
     <section className="flex flex-col items-center py-16">
       <div className="StripContainer grid max-w-screen-xl grid-cols-4 gap-x-2 p-4">
-        <div className="FilmStrip">
-          <div className="mb-2">
-            <img
-              className="hover:cursor-pointer"
-              src={images.photo1}
-              loading="lazy"
-              alt=""
-            />
-          </div>
-          <div className="mb-2">
-            <img
-              className="hover:cursor-pointer"
-              src={images.photo2}
-              loading="lazy"
-              alt=""
-            />
-          </div>
-          <div className="mb-2">
-            <img
-              className="hover:cursor-pointer"
-              src={images.photo3}
-              loading="lazy"
-              alt=""
-            />
-          </div>
-          <div className="mb-2">
-            <img
-              className="hover:cursor-pointer"
-              src={images.photo4}
-              loading="lazy"
-              alt=""
-            />
-          </div>
+        <div className="filmStrip">
+          {data.stripOne.map((item, index) => (
+            <div key={index} className="mb-2">
+              <img
+                className="hover:cursor-pointer"
+                loading="lazy"
+                src={item.link}
+                alt={item.text}
+              />
+            </div>
+          ))}
         </div>
-
-        <div className="FilmStrip">
-          <div className="mb-2">
-            <img
-              className="hover:cursor-pointer"
-              src={images.photo5}
-              loading="lazy"
-              alt=""
-            />
-          </div>
-          <div className="mb-2">
-            <img
-              className="hover:cursor-pointer"
-              src={images.photo6}
-              loading="lazy"
-              alt=""
-            />
-          </div>
-          <div className="mb-2">
-            <img
-              className="hover:cursor-pointer"
-              src={images.photo7}
-              loading="lazy"
-              alt=""
-            />
-          </div>
-          <div className="mb-2">
-            <img
-              className="hover:cursor-pointer"
-              src={images.photo8}
-              loading="lazy"
-              alt=""
-            />
-          </div>
+        <div className="filmStrip">
+          {data.stripTwo.map((item, index) => (
+            <div key={index} className="mb-2">
+              <img
+                className="hover:cursor-pointer"
+                loading="lazy"
+                src={item.link}
+                alt={item.text}
+              />
+            </div>
+          ))}
         </div>
-
-        <div className="FilmStrip">
-          <div className="mb-2">
-            <img
-              className="hover:cursor-pointer"
-              src={images.photo9}
-              loading="lazy"
-              alt=""
-            />
-          </div>
-          <div className="mb-2">
-            <img
-              className="hover:cursor-pointer"
-              src={images.photo10}
-              loading="lazy"
-              alt=""
-            />
-          </div>
-          <div className="mb-2">
-            <img
-              className="hover:cursor-pointer"
-              src={images.photo11}
-              loading="lazy"
-              alt=""
-            />
-          </div>
-          <div className="mb-2">
-            <img
-              className="hover:cursor-pointer"
-              src={images.photo12}
-              loading="lazy"
-              alt=""
-            />
-          </div>
+        <div className="filmStrip">
+          {data.stripThree.map((item, index) => (
+            <div key={index} className="mb-2">
+              <img
+                className="hover:cursor-pointer"
+                loading="lazy"
+                src={item.link}
+                alt={item.text}
+              />
+            </div>
+          ))}
         </div>
-
-        <div className="FilmStrip">
-          <div className="mb-2">
-            <img
-              className="hover:cursor-pointer"
-              src={images.photo13}
-              loading="lazy"
-              alt=""
-            />
-          </div>
-          <div className="mb-2">
-            <img
-              className="hover:cursor-pointer"
-              src={images.photo14}
-              loading="lazy"
-              alt=""
-            />
-          </div>
-          <div className="mb-2">
-            <img
-              className="hover:cursor-pointer"
-              src={images.photo15}
-              loading="lazy"
-              alt=""
-            />
-          </div>
-          <div className="mb-2">
-            <img
-              className="hover:cursor-pointer"
-              src={images.photo16}
-              loading="lazy"
-              alt=""
-            />
-          </div>
+        <div className="filmStrip">
+          {data.stripFour.map((item, index) => (
+            <div key={index} className="mb-2">
+              <img
+                className="hover:cursor-pointer"
+                loading="lazy"
+                src={item.link}
+                alt={item.text}
+              />
+            </div>
+          ))}
         </div>
       </div>
     </section>
